71c3c2077f0e28374a2ce5dddc62385ab9ff6172
Karen Etheridge <ether@cpan.org>
2014-12-07 16:45:13 -0800

    initial commit
    via [Git::Init] in Dist-Zilla-PluginBundle-Author-ETHER-0.080, github profile.

---
+++ lib/Acme/Our/Version.pm
@@ -0,0 +1,47 @@
+use strict;
+use warnings;
+package Acme::Our::Version;
+# ABSTRACT: ...
+# KEYWORDS: ...
+# vim: set ts=8 sw=4 tw=78 et :
+
+
+1;
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Acme::Our::Version;
+
+    ...
+
+=head1 DESCRIPTION
+
+...
+
+=head1 FUNCTIONS/METHODS
+
+=head2 C<foo>
+
+...
+
+=head1 SUPPORT
+
+=for stopwords irc
+
+Bugs may be submitted through L<the RT bug tracker|https://rt.cpan.org/Public/Dist/Display.html?Name=Acme-Our-Version>
+(or L<bug-Acme-Our-Version@rt.cpan.org|mailto:bug-Acme-Our-Version@rt.cpan.org>).
+I am also usually active on irc, as 'ether' at C<irc.perl.org>.
+
+=head1 ACKNOWLEDGEMENTS
+
+...
+
+=head1 SEE ALSO
+
+=for :list
+* L<foo>
+
+=cut
---
+++ t/01-basic.t
@@ -0,0 +1,9 @@
+use strict;
+use warnings FATAL => 'all';
+
+use Test::More;
+use if $ENV{AUTHOR_TESTING}, 'Test::Warnings';
+use Acme::Our::Version;
+
+fail('this test is TODO!');
+done_testing;
